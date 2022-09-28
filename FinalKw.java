66524193f72e3f2e85d37aece725b289d4b8ac31
Haja Mohideen <97336864+haja1221@users.noreply.github.com>
2022-09-28 09:39:28 +0530

    Add files via upload

---
+++ FinalKw.java
@@ -0,0 +1,12 @@
+class FinalKw{
+    void changeName(String manish){
+    manish ="kesavan";
+    System.out.println(manish);    
+
+    }
+     public static void main(String[]args){
+      FinalKw haja= new FinalKw();
+         final String manish="manish_mani";
+         haja.changeName(manish);
+   }
+}
